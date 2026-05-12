6eb66210bb0af41a667f345c3e4db9bd77648f5e
Clinton <nyaenyaklinton@gmail.com>
2026-05-12 20:46:27 +0300

    Update on extract gas prices logic

---
+++ extract.py
@@ -0,0 +1,31 @@
+import http.client
+import json
+from pathlib import Path
+
+def extract():
+    conn = http.client.HTTPSConnection("api.collectapi.com")
+
+    headers = {
+        'content-type': "application/json",
+        'authorization': "apikey 3De19gvsnyPt5ZSkm1RFSR:7iPAECJDV3dhSjptqtkeaR"
+    }
+
+    conn.request("GET", "/gasPrice/stateUsaPrice?state=WA", headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    parsed_data = json.loads(data.decode("utf-8"))
+
+    # current project directory
+    project_root = Path(__file__).resolve().parent
+
+    file_path = project_root / "gas_prices.json"
+
+    with open(file_path, "w") as f:
+        json.dump(parsed_data, f, indent=4)
+
+    print(f"Saved to: {file_path}")
+
+if __name__ == "__main__":
+    extract()
